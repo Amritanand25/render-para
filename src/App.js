2429fec9d069010c83b9da013bbd870017a743eb
Amrit Anand <48414060+Amritanand25@users.noreply.github.com>
2021-06-19 21:46:00 +0530

    updated
    commit updated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
 
-function App() {
+const App = () => {
+  function inputHandler(e) {
+    let data = e.target.value;
+    localStorage.setItem("name", `${data}`);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        type="text"
+        onChange={(e) => {
+          inputHandler(e);
+        }}
+      />
     </div>
   );
-}
+};
 
 export default App;
